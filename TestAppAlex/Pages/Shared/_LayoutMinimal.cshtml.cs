4945a5fb320c52e1d2759931813e9c289a8a1d04
GaLeX13 <gheorghea541@gmail.com>
2025-05-05 23:20:56 +0300

    Chesti care nu si au dat commit din prima :)

---
+++ TestAppAlex/Pages/Shared/_LayoutMinimal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestAppAlex.Pages.Shared
+{
+    public class _LayoutMinimalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
